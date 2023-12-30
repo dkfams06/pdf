016630e91727a1082d58c0b7b96b241eef794c07
dkfams06 <93527840+dkfams06@users.noreply.github.com>
2023-12-30 16:37:42 +0900

    Add files via upload

--- pdf.py
+++ pdf.py
@@ -1,80 +1,82 @@
-#기본정보
-
 import streamlit as st
-#pdf리더
 from PyPDF2 import PdfReader
-#랭체인
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
-#구글번역
 from googletrans import Translator
+from langchain.vectorstores.faiss_vector_store import FAISS
 
+# Function to perform Google Translate
+def google_translate(messages):
+    translator = Translator()
+    result = translator.translate(messages, dest="ko")
+    return result.text
 
-# 기능구현
-def google_trans(messages):
-    google = Translator()
-    resurt = google.Translate(messages, dest="ko")
-    return ressult.text
-
-# 메인함수
+# Main function
 def main():
-    st.set_page_config(page_title="PDF analyzer", layout="wide")
+    st.set_page_config(page_title="PDF Analyzer", layout="wide")
 
-    #사이드바
-    with st.sidenar:
-        open_apikey=st.text_input(label='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',placeholder='enter your ap')
+    # Sidebar
+    with st.sidebar:
+        open_apikey = st.text_input(label='OpenAI API Key', placeholder='Enter your API key')
         if open_apikey:
-            st.session_state["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"]=open_apikey
+            st.session_state["openai_api"] = open_apikey
         st.markdown('---')
-    
-    #메인공간
-    st.header("pdf 내용 질문 프로그램")
+
+    # Main space
+    st.header("PDF Content Questioning Program")
     st.markdown('---')
-    st.subheader("pdf 파일을 넣으세요")
+    st.subheader("Upload a PDF File")
     
-    #pdf파일받기
-    pdf=st.file_uploader(" ", type="pdf")
+    # Upload PDF file
+    pdf = st.file_uploader(" ", type="pdf")
+    
     if pdf is not None:
-        pdf_reader=PdfReader(pdf)
-        text=""
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        
         for page in pdf_reader.pages:
-            text+=page.extract_text()
-        text_splitter=CharacterTextSplitter(
+            text += page.extract_text()
+        
+        text_splitter = CharacterTextSplitter(
             separator="\n",
             chunk_size=1000,
             chunk_overlap=200,
             length_function=len
         )
-        chunks=text_splitter.split_text(text)
+        chunks = text_splitter.split_text(text)
 
         st.markdown('---')
-        st.subheader("질문을 입력하세요")
-        #질문받기
-        user_question=st.text_input("Ask question :")
+        st.subheader("Enter Your Question")
+        
+        # Get user question
+        user_question = st.text_input("Ask a question:")
+        
         if user_question:
-            #임베딩/시멘틱 인덱스
-            embeddings=OpenAIEmbeddings(openai_api_key=st.session_state["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"])
-            knowledge_base=FAISS.from_texts(chunks,embeddings)
-            docs=knowledge_base.similarity_search(user_question)
+            # Embeddings/Semantic Index
+            embeddings = OpenAIEmbeddings(openai_api_key=st.session_state["openai_api"])
+            knowledge_base = FAISS.from_texts(chunks, embeddings)
+            docs = knowledge_base.similarity_search(user_question)
 
-            #질문
-            lim=ChatOpenAI(temperature=0,
-                    openai_api_key=st.session_state["open_api"],
-                    max_tokens=2000,
-                    model_name='gpt-3.5-turbo',
-                    request_timeout=120
-                    )
-            chain=load_qa_chain(lim,chain_type="stuff")
-            response=chain.run(input_documents=docs,question=user_question)
-                #답변결과
+            # Questioning
+            lim = ChatOpenAI(
+                temperature=0,
+                openai_api_key=st.session_state["openai_api"],
+                max_tokens=2000,
+                model_name='gpt-3.5-turbo',
+                request_timeout=120
+            )
+            chain = load_qa_chain(lim, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=user_question)
+            
+            # Display the answer
             st.info(response)
-            #번역
-            if st.button(label="번역하기"):
-                trans=google_trans(response)
-                st.success(trans)
+            
+            # Translation
+            if st.button(label="Translate"):
+                translation = google_translate(response)
+                st.success(translation)
 
-    if _name_=='__main__':
-        main()
+if __name__ == '__main__':
+    main()
